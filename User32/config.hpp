21e13ad02d214eb3f0aeefa224aecb0f2febd874
ShadowMonster <81448108+ShadowMonster99@users.noreply.github.com>
2023-05-01 17:23:18 -0300

    Add files via upload

---
+++ User32/config.hpp
@@ -0,0 +1,124 @@
+#ifndef CONFIG
+#define CONFIG
+
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <Lmcons.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <winsock2.h>
+#include <chrono>
+
+class SkinConfig
+{
+private:
+    std::string SteamSkinPath;
+    std::string currentSkin;
+public:
+
+    SkinConfig(const SkinConfig&) = delete;
+    SkinConfig& operator=(const SkinConfig&) = delete;
+
+    static SkinConfig& getInstance()
+    {
+        static SkinConfig instance;
+        return instance;
+    }
+
+    SkinConfig()
+    {
+        HKEY hKey;
+        DWORD dwType = REG_SZ, dwSize = MAX_PATH;
+        TCHAR szInstallPath[MAX_PATH];
+
+        auto handleError = [&](const std::string& message) -> void {
+            std::cout << "couldn't get steam path, assuming steam path (hope for the best)" << std::endl;
+            SteamSkinPath = "C:\\Program Files (x86)\\Steam\\";
+        };
+
+        if (RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("Software\\Valve\\Steam"), 0, KEY_READ, &hKey) != ERROR_SUCCESS)
+            handleError("Failed to open Steam key");
+
+        if (RegGetValue(hKey, NULL, TEXT("SteamPath"), RRF_RT_REG_SZ, &dwType, szInstallPath, &dwSize) != ERROR_SUCCESS)
+            handleError("Failed to read InstallPath value");
+
+        RegCloseKey(hKey);
+
+        SteamSkinPath = std::format("{}/steamui/skins", szInstallPath);
+    }
+
+    nlohmann::json GetConfig()
+    {
+        std::ifstream configFile(std::format("{}/settings.json", SteamSkinPath));
+        if (!configFile.is_open() || !configFile) {
+            std::cout << "Failed to open config file!" << std::endl;
+        }
+
+        std::stringstream buffer;
+        buffer << configFile.rdbuf();
+
+        if (nlohmann::json::accept(buffer.str())) {
+            nlohmann::json json_object = nlohmann::json::parse(buffer.str());
+            currentSkin = json_object["active-skin"];
+            return json_object;
+        }
+        else {
+
+            std::string path = std::format("{}/settings.json", SteamSkinPath);
+
+            MessageBoxA(0, std::format("json from \n[{}] is invalid.\nits not likely an extra , at the end of the json", path).c_str(), "Millennium", MB_ICONINFORMATION);
+            __fastfail(0);
+        }
+    }
+
+    nlohmann::json GetSkinConfig()
+    {
+        {
+            std::ifstream configFile(std::format("{}/settings.json", SteamSkinPath));
+            if (!configFile.is_open() || !configFile) {
+                std::cout << "Failed to open config file!" << std::endl;
+            }
+
+            std::stringstream buffer;
+            buffer << configFile.rdbuf();
+
+            if (nlohmann::json::accept(buffer.str())) {
+                nlohmann::json json_object = nlohmann::json::parse(buffer.str());
+                currentSkin = json_object["active-skin"];
+            }
+            else {
+
+                std::string path = std::format("{}/settings.json", SteamSkinPath);
+
+                MessageBoxA(0, std::format("json from \n[{}] is invalid.\nits not likely an extra , at the end of the json", path).c_str(), "Millennium", MB_ICONINFORMATION);
+                __fastfail(0);
+            }
+        }
+        {
+            std::ifstream configFile(std::format("{}/{}/config.json", SteamSkinPath, currentSkin));
+            if (!configFile.is_open() || !configFile) {
+                std::cout << "Failed to open config file!" << std::endl;
+            }
+
+            std::stringstream buffer;
+            buffer << configFile.rdbuf();
+
+            if (nlohmann::json::accept(buffer.str())) {
+                nlohmann::json json_object = nlohmann::json::parse(buffer.str());
+                return json_object;
+            }
+            else {
+                std::string path = std::format("{}/{}/config.json", SteamSkinPath, currentSkin);
+
+                MessageBoxA(0, std::format("json from \n[{}] is invalid.\nits not likely an extra , at the end of the json", path).c_str(), "Millennium", MB_ICONINFORMATION);
+                __fastfail(0);
+            }
+        }
+    }
+};
+
+#endif // CONFIG
---
+++ User32/logger.hpp
@@ -0,0 +1,42 @@
+#ifndef LOGGER
+#define LOGGER
+
+class Console {
+private:
+	std::string get_time()
+	{
+		// Get the current time
+		std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+		std::time_t time = std::chrono::system_clock::to_time_t(now);
+
+		// Convert the time to a tm struct
+		std::tm tm = *std::localtime(&time);
+
+		std::stringstream ss;
+		ss << std::put_time(&tm, "%y-%m-%d %H:%M:%S");
+		return "[" + ss.str() + "]";
+	}
+public:
+	void log(std::string val)
+	{
+		//std::cout << get_time() << " [INFO] " << val << std::endl;
+	}
+	void err(std::string val)
+	{
+		//std::cout << get_time() << " [FAIL] " << val << std::endl;
+	}
+	void warn(std::string val)
+	{
+		//std::cout << get_time() << " [WARN] " << val << std::endl;
+	}
+	void succ(std::string val)
+	{
+		//std::cout << get_time() << " [OKAY] " << val << std::endl;
+	}
+	void imp(std::string val)
+	{
+		std::cout << get_time() << " [Millennium] " << val << std::endl;
+	}
+};
+
+#endif // LOGGER
