4297385a934314c3c97c617f25127bee25fc3a6d
Rose <4759227+RoseTheFlower@users.noreply.github.com>
2024-01-17 01:05:14 +0500

    Another missing comma, I think

--- src/utils/config/config.cpp
+++ src/utils/config/config.cpp
@@ -455,7 +455,7 @@ const nlohmann::json themeConfig::getThemeData(bool raw) noexcept
     if (hasJavaScriptPatch && Settings::Get<bool>("allow-javascript") == false) {
         int result = MsgBox(
             "The selected theme may be using JavaScript to enhance your Steam experience.\n"
-            "You have JavaScript disabled in Millennium settings, therefore the selected skin may not function properly.\n\n"
+            "You have JavaScript disabled in Millennium settings, therefore, the selected skin may not function properly.\n\n"
             "Enable JavaScript ONLY IF you trust the developer, have manually reviewed the code, or it's an official theme.\n\n"
             "Would you like to enable JavaScript execution?",
             "Notice", 
