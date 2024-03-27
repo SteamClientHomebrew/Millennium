e3b547fd55d278dee15eb3814c1da245d77f00f4
ShadowMonster99 <81448108+ShadowMonster99@users.noreply.github.com>
2024-03-27 16:25:21 -0300

    rename update mng

---
+++ interop/manager/HashManager.cs
@@ -0,0 +1,139 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using updater.utils;
+public class HashObject
+{
+    public string hash { get; set; }
+    public string commit { get; set; }
+}
+
+internal class HashManager
+{
+    private static readonly string filePath = ".millennium/manager/hashes-backend.json";
+
+    private static bool isValid(string s)
+    {
+        try
+        {
+            JToken.Parse(s);
+            return true;
+        }
+        catch (JsonReaderException)
+        {
+            return false;
+        }
+    }
+    
+    public static bool GetHashFromTheme(string owner, string repo, string currentHash, out string hash)
+    {
+        string hashedName = Hash.Evaluate(owner + repo);
+
+        try
+        {
+            List<HashObject> hashObjects;
+
+            if (File.Exists(filePath))
+            {
+                string jsonContent = File.ReadAllText(filePath);
+
+                if (!string.IsNullOrEmpty(jsonContent) && isValid(jsonContent))
+                {
+                    // Deserialize the JSON content into a list of HashObjects
+                    hashObjects = JsonConvert.DeserializeObject<List<HashObject>>(jsonContent);
+
+                    // Check if the hash name exists in the list
+                    var existingHashObject = hashObjects?.Find(obj => obj.hash == hashedName);
+                    if (existingHashObject != null)
+                    {
+                        // The hash name exists, return its hash data
+                        hash = existingHashObject.commit;
+                        return true;
+                    }
+                }
+                else
+                {
+                    // If the file exists but has no valid JSON data, initialize the list
+                    hashObjects = new List<HashObject>();
+                }
+            }
+            else
+            {
+                // If the file doesn't exist, initialize the list
+                hashObjects = new List<HashObject>();
+            }
+
+            // Add a new HashObject to the list
+            hashObjects.Add(new HashObject { hash = hashedName, commit = currentHash });
+
+            // Serialize the list back to JSON
+            string updatedJsonContent = JsonConvert.SerializeObject(hashObjects, Formatting.Indented);
+
+            try
+            {
+                // Create directories recursively
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            // Write to the file (create if it doesn't exist)
+            File.WriteAllText(filePath, updatedJsonContent);
+
+            // Set the out variable
+            hash = currentHash;
+            return false;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading or deserializing/serializing JSON: {ex.Message}");
+            hash = null;
+            return false;
+        }
+    }
+
+    public static void SetHash(string owner, string repo, string hashData)
+    {
+        string hashedName = Hash.Evaluate(owner + repo);
+
+        Logger.Log($"[+] Attempting to set new hash for: {hashedName}");
+
+        try
+        {
+            List<HashObject> hashObjects;
+
+            if (File.Exists(filePath))
+            {
+                string jsonContent = File.ReadAllText(filePath);
+                hashObjects = JsonConvert.DeserializeObject<List<HashObject>>(jsonContent);
+            }
+            else
+            {
+                hashObjects = new List<HashObject>();
+            }
+
+            var existingHashObject = hashObjects?.Find(obj => obj.hash == hashedName);
+
+            if (existingHashObject != null)
+            {
+                existingHashObject.commit = hashData;
+            }
+            else
+            {
+                hashObjects.Add(new HashObject { hash = hashedName, commit = hashData });
+            }
+
+            string updatedJsonContent = JsonConvert.SerializeObject(hashObjects, Formatting.Indented);
+            File.WriteAllText(filePath, updatedJsonContent);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading or deserializing/serializing JSON: {ex.Message}");
+        }
+    }
+}
---
+++ interop/utils/HttpClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace updater
+{
+    internal class Http
+    {
+        private static void HandleError(string message)
+        {
+            Logger.Log($"Error making GET request: {message}");
+
+            if (message.Contains("403 (rate limit exceeded)"))
+            {
+                MessageBox.Show("Github API has a rate limit of 60rph which you have succeeded. Please wait at max an hour and retry", "Updater - Rate Limited", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public static async Task<string> Get(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("Millennium.Updater");
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException ex)
+                {
+                    HandleError(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    HandleError(ex.Message);
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
