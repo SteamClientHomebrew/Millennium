9d1a475b1f55f2875efae022d5e535e0363da083
ShadowMonster99 <81448108+ShadowMonster99@users.noreply.github.com>
2024-05-19 22:01:14 -0300

    core(clean): minify

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -30,13 +30,19 @@ const bool __attribute__((constructor)) setup_env()
         void(freopen("CONOUT$", "w", stderr));
     }
 #endif
-    const char* filePath = (stream_buffer::steam_path() / ".cef-enable-remote-debugging").generic_string().c_str();
+
+#ifdef _WIN32
+    constexpr const char* filePath = ".cef-enable-remote-debugging";
+#elif __linux__
+    std::string filePath = fmt::format("{}/.local/share/Steam/.cef-enable-remote-debugging", std::getenv("HOME"));
+#endif
 
     if (!std::filesystem::exists(filePath)) {
         std::ofstream(filePath).close();
-        boxer::show("Successfully initialized Millennium!. You can now manually restart Steam...", "Message");  
-        std::exit(0);
 
+        boxer::show("Successfully initialized Millennium!. You can now manually restart Steam...", "Message");
+        
+        std::exit(0);
         return false;
     }
     return true;
