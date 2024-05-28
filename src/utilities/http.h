d13f61f209fa8d00bd89dddddb0f206fcf6bfc92
ShadowMonster99 <81448108+ShadowMonster99@users.noreply.github.com>
2024-05-28 13:50:22 -0300

    core(impl): fix memory bug

--- src/utilities/http.h
+++ src/utilities/http.h
@@ -9,7 +9,7 @@ static std::string http_get(const char* url) {
     cpr::Response response = cpr::Get(cpr::Url{url}, cpr::UserAgent{"millennium.patcher/1.0"});
 
     if (response.error) {
-        console.err("[http] {}", response.error.message);
+        //console.err("[http] {}", response.error.message);
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
         return http_get(url);
     }
