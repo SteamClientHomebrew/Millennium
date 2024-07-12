d8f304b5e97ef542e6228c45d891906c4e3f6cb2
shadow <81448108+ShadowMonster99@users.noreply.github.com>
2024-07-12 17:09:10 -0300

    scripts - update installer

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,71 +1,98 @@
-# Get the process ID (PID) of steam.exe
-$steamProcess = Get-Process -Name "steam" -ErrorAction SilentlyContinue
+# Define ANSI escape sequence for bold purple color
+$BoldPurple = [char]27 + '[38;5;219m'
+$BoldGreen = [char]27 + '[1;32m'
+$BoldLightBlue = [char]27 + '[38;5;75m'
 
-# Check if steam.exe is running
-if ($steamProcess) {
-    # Kill steam.exe process
-    Stop-Process -Name "steam" -Force
-    Write-Output "[+] Closed Steam process."
+$ResetColor = [char]27 + '[0m'
+
+function Close-SteamProcess {
+    $steamProcess = Get-Process -Name "steam" -ErrorAction SilentlyContinue
+
+    if ($steamProcess) {
+        Stop-Process -Name "steam" -Force
+        Write-Output "[+] Closed Steam process."
+    }
+}
+
+# Kill steam process before installing
+Close-SteamProcess
+
+function ConvertTo-ReadableSize {
+    param([int64]$size)
+    
+    $units = @("B", "KiB", "MiB", "GiB")
+    $index = [Math]::Floor([Math]::Log($size, 1024))
+    $sizeFormatted = [Math]::Round($size / [Math]::Pow(1024, $index), 2, [MidpointRounding]::AwayFromZero)
+    
+    return "$sizeFormatted $($units[$index])"
+}
+
+function Update-Config {
+    param([string]$JsonFilePath, [string]$Key, $Value)
+
+    $directory = Split-Path -Path $JsonFilePath
+    if (-not (Test-Path $directory)) {
+        New-Item -Path $directory -ItemType Directory -Force -ErrorAction Stop | Out-Null
+    }
+    if (-not (Test-Path $JsonFilePath) -or (Get-Content $JsonFilePath -Raw) -eq '') {
+        @{} | ConvertTo-Json | Set-Content -Path $JsonFilePath -Force -ErrorAction Stop
+    }
+    $jsonContent = Get-Content -Path $JsonFilePath -Raw | ConvertFrom-Json -ErrorAction Stop
+
+    try {
+        $jsonContent | Add-Member -MemberType NoteProperty -Name $Key -Value $Value -ErrorAction Stop
+    }
+    catch {
+        $jsonContent.$Key = $Value
+    }
+    ($jsonContent | ConvertTo-Json -Depth 6).Replace('  ',' ') | Out-File $JsonFilePath -Force -ErrorAction Stop
 }
 
 function Show-Progress {
-    param (
-        [int]$FileNumber,
-        [int]$TotalFiles,
-        [int]$PercentComplete,
-        [string]$Message = "Downloading..."
-    )
+    param ([int]$FileNumber, [int]$TotalFiles, [int]$PercentComplete, [string]$Message, [int]$CurrentRead, [int]$TotalBytes)
 
-    $ProgressBarLength = 25
+    $ProgressBarLength = 65
     $Progress = [math]::round(($PercentComplete / 100) * $ProgressBarLength)
     $ProgressBar = ("#" * $Progress).PadRight($ProgressBarLength)
     
-    $FileCountMessage = "[$FileNumber/$TotalFiles]"
-    $ProgressMessage = "`r$FileCountMessage [$ProgressBar] $PercentComplete% - $Message"
+    $FileCountMessage = "${BoldPurple}[$FileNumber/$TotalFiles]${ResetColor}"
+    $ProgressMessage = "$FileCountMessage $Message"
+    $placement = ([Console]::WindowWidth + 6) - $ProgressBarLength
+    $addLength = $placement - $ProgressMessage.Length
+    $spaces = -join (' ' * $addLength)
     
-    Write-Host -NoNewline $ProgressMessage -f Magenta
+    $currentDownload = ConvertTo-ReadableSize -size $CurrentRead
+    $backspace = "`b" * $currentDownload.Length
+
+    # [Console]::SetCursorPosition($placement, [Console]::CursorTop)
+    Write-Host -NoNewline "`r$ProgressMessage$spaces${BoldGreen}${backspace}${currentDownload} [$ProgressBar]${ResetColor} $PercentComplete%"
 }
 
 function DownloadFileWithProgress {
-    param (
-        [string]$Url,
-        [string]$DestinationPath,
-        [int]$FileNumber,
-        [int]$TotalFiles,
-        [string]$FileName
-    )
+    param ([string]$Url, [string]$DestinationPath, [int]$FileNumber, [int]$TotalFiles, [string]$FileName)
 
     try {
-        # Create the web request
         $webRequest = [System.Net.HttpWebRequest]::Create($Url)
         $webRequest.Method = "GET"
         $webRequest.UserAgent = "Millennium.Installer/1.0"
 
-        # Get the response
         $response = $webRequest.GetResponse()
         $totalBytes = $response.ContentLength
         $responseStream = $response.GetResponseStream()
 
-        # Create the output file
         $fileStream = New-Object System.IO.FileStream($DestinationPath, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write, [System.IO.FileShare]::None)
         $buffer = New-Object byte[] 8192
         $bytesRead = 0
         $totalBytesRead = 0
 
-        [Console]::CursorTop -= 0
+        Write-Host "`r"
 
-        [Console]::SetCursorPosition(0, [Console]::CursorTop)
-        [Console]::Write(' ' * [Console]::WindowWidth)
-
-        # Download in chunks
         while (($bytesRead = $responseStream.Read($buffer, 0, $buffer.Length)) -gt 0) {
             $fileStream.Write($buffer, 0, $bytesRead)
             $totalBytesRead += $bytesRead
             $percentComplete = [math]::round(($totalBytesRead / $totalBytes) * 100)
-            Show-Progress -FileNumber $FileNumber -TotalFiles $TotalFiles -PercentComplete $percentComplete -Message $FileName
+            Show-Progress -FileNumber $FileNumber -TotalFiles $TotalFiles -PercentComplete $percentComplete -Message $FileName -CurrentRead $totalBytesRead -TotalBytes $totalBytes
         }
-
-        # Cleanup
         $fileStream.Close()
         $responseStream.Close()
     }
@@ -76,100 +103,80 @@ function DownloadFileWithProgress {
     }
 }
 
-# Define the GitHub API URL
 $apiUrl = "https://api.github.com/repos/SteamClientHomebrew/Millennium/releases"
-
-# Make the API request
 $response = Invoke-RestMethod -Uri $apiUrl -Headers @{ "User-Agent" = "Millennium.Installer/1.0" }
-
-# Filter the releases to get the latest non-prerelease release
 $latestRelease = $response | Where-Object { -not $_.prerelease } | Sort-Object -Property created_at -Descending | Select-Object -First 1
 
-# Prompt the user for a custom Steam path
-$customSteamPath = Read-Host "[?] Steam Path (leave blank for default)"
+$customSteamPath = Read-Host "${BoldPurple}[?]${ResetColor} Steam Path (leave blank for default)"
 
 if (-not $customSteamPath) {
-    # Get the Steam path from the registry if no custom path is provided
     $steamPath = (Get-ItemProperty -Path "HKCU:\Software\Valve\Steam").SteamPath
 
     if (-not $steamPath) {
         Write-Host "Steam path not found in registry."
         exit
     }
+    
+    [Console]::CursorTop -= 1
+
+    [Console]::SetCursorPosition(0, [Console]::CursorTop)
+    [Console]::Write(' ' * [Console]::WindowWidth)
+
+    Write-Output "`r${BoldPurple}[?]${ResetColor} Steam Path (leave blank for default): ${BoldLightBlue}$steamPath${ResetColor}"
+
 } else {
     $steamPath = $customSteamPath
 }
 
-Write-Output "[+] Using Steam path: $steamPath"
+#Write-Output "${BoldPurple}[+]${ResetColor} Using Steam path: ${BoldLightBlue}$steamPath${ResetColor}"
 
 # Ensure the destination directory exists
 if (-not (Test-Path -Path $steamPath)) {
     New-Item -ItemType Directory -Path $steamPath
 }
 
-# Calculate the total size of all download assets in bytes
-$totalSizeBytes = ($latestRelease.assets | Measure-Object -Property size -Sum).Sum
+# Convert the total size to a readable format
+$totalSizeReadable = ConvertTo-ReadableSize -size ($latestRelease.assets | Measure-Object -Property size -Sum).Sum
+$releaseTag = $latestRelease.tag_name
+$packageCount = $latestRelease.assets.Count
 
-# Function to convert bytes to the appropriate unit (KB, MB, GB)
-function ConvertTo-ReadableSize {
-    param([int64]$size)
-    
-    $units = @("bytes", "KB", "MB", "GB")
-    $index = [Math]::Floor([Math]::Log($size, 1024))
-    $sizeFormatted = [Math]::Round($size / [Math]::Pow(1024, $index), 2)
-    
-    return "$sizeFormatted $($units[$index])"
+Write-Output "`n${BoldPurple}[+]${ResetColor} Packages ($packageCount) ${BoldPurple}Millennium@$releaseTag${ResetColor}`n"
+Write-Output "${BoldPurple}[+]${ResetColor} Total Download Size:  $totalSizeReadable"
+Write-Output "${BoldPurple}[+]${ResetColor} Total Installed Size: $totalSizeReadable"
+
+# offer to proceed with installation
+$choice = Read-Host "`n${BoldPurple}::${ResetColor} Proceed with installation? [Y/n]"
+
+if ($choice -eq "n" -or $choice -eq "no") {
+    Write-Output "${BoldPurple}[+]${ResetColor} Installation aborted."
+    exit
 }
 
-# Convert the total size to a readable format
-$totalSizeReadable = ConvertTo-ReadableSize -size $totalSizeBytes
+Write-Host "${BoldPurple}::${ResetColor} Retreiving packages..."
 
-# Print the total size
-Write-Output "[+] Downloading [$totalSizeReadable] of required files..."
-
-# Write-Host "[+] Downloading required files... $latestRelease"
-
-# Download each asset in the release
 $FileCount = $latestRelease.assets.Count
 for ($i = 0; $i -lt $FileCount; $i++) {
     $asset = $latestRelease.assets[$i]
     $downloadUrl = $asset.browser_download_url
     $outputFile = Join-Path -Path $steamPath -ChildPath $asset.name
 
-    # Download the file with custom progress bar
     DownloadFileWithProgress -Url $downloadUrl -DestinationPath $outputFile -FileNumber ($i + 1) -TotalFiles $FileCount -FileName $asset.name
 }
 
-# Prompt the user for input with formatted text
-$choice = Read-Host "`n[+] Do you want to install developer packages? [y/N]"
-
-# Process user input
-if ($choice -eq "y" -or $choice -eq "yes") {
-    Write-Host "yes"
-} 
-elseif ($choice -eq "n" -or $choice -eq "no") {
-    Write-Host "no"
-} 
+Write-Host "`n"
 
 
-# Prompt the user for input with formatted text
-$choice = Read-Host "[+] Do you want Millennium to auto update? (Recommeded) [Y/n]"
+# $choice = Read-Host "`n${BoldPurple}[?]${ResetColor} Do you want to install developer packages? [y/N]"
+# if ($choice -eq "y" -or $choice -eq "yes") {
+#     Write-Host "yes"
+# } 
 
-# Process user input
-if ($choice -eq "y" -or $choice -eq "yes") {
-    Write-Host "yes"
-} 
-elseif ($choice -eq "n" -or $choice -eq "no") {
-    Write-Host "no"
-} 
+# $choice = Read-Host "${BoldPurple}[?]${ResetColor} Do you want Millennium to auto update? (Recommeded) [Y/n]"
+# if ($choice -ne "n" -or $choice -ne "no") {
+#     Update-Config -Key "autoUpdate" -Value $true -JsonFilePath (Join-Path -Path $steamPath -ChildPath "/ext1/plugins.json")
+# }
 
-# Prompt the user for input with formatted text
-$choice = Read-Host "[+] Do you want to use Millennium's GUI? (no = configure millennium with files only) [Y/n]"
-
-# Process user input
-if ($choice -eq "y" -or $choice -eq "yes") {
-    Write-Host "yes"
-} 
-elseif ($choice -eq "n" -or $choice -eq "no") {
-    Write-Host "no"
-} 
+# $choice = Read-Host "${BoldPurple}[?]${ResetColor} Do you want to use Millennium's GUI? (no = configure millennium with files only) [Y/n]"
+# if ($choice -ne "n" -or $choice -ne "no") {
+#     Update-Config -Key "gui" -Value $true -JsonFilePath (Join-Path -Path $steamPath -ChildPath "/ext1/plugins.json")
+# } 
