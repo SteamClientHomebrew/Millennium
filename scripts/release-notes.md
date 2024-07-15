1ff314a8a1ed719a93dd0516e429a60a97738f8b
shadow <81448108+ShadowMonster99@users.noreply.github.com>
2024-07-15 02:26:38 -0300

    version - bump to

---
+++ scripts/release-notes.md
@@ -0,0 +1,5 @@
+## What's Changed
+
+* Auto Updater for new version
+* Bug fixes on Millennium startup
+* Fix Steam crash on PC Shutdown?
--- scripts/release.ps1
+++ scripts/release.ps1
@@ -3,13 +3,21 @@ $version = $content[1]
 
 $response = Read-Host "[Millennium@$version] new version"
 
-Write-Host "You entered: $response"
-
 if ($response -ne "") {
     $content[1] = $response
 }
 # Write the modified content back to the file
 $content | Set-Content -Path $file
+
+$releaseMarkdown = "./scripts/release-notes.md"
+
+if (!(Test-Path $releaseMarkdown)) {
+    New-Item -ItemType File -Path $releaseMarkdown > $null
+} else {
+    Clear-Content -Path $releaseMarkdown
+}
+
+$response = Read-Host "Created release notes, press enter to continue"
 
 git add .
 git commit -m "version - bump to $response"
