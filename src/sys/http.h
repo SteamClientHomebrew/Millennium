2370306f6ba0076d345df51c1272315dec916f22
shadow <81448108+ShadowMonster99@users.noreply.github.com>
2024-07-20 03:39:10 -0300

    chore: switch to libcurl instead of cpr

--- src/sys/http.h
+++ src/sys/http.h
@@ -2,21 +2,89 @@
 #include <string>
 #include <chrono>
 #include <thread>
-#include <cpr/cpr.h>
 #include <sys/log.h>
+#include <curl/curl.h>
 
-static std::string GetRequest(const char* url) 
+static size_t write_callback(char* ptr, size_t size, size_t nmemb, std::string* data) 
 {
-    while (true) 
-    {
-        cpr::Response response = cpr::Get(cpr::Url{url}, cpr::UserAgent{"millennium.patcher/1.0"});
+    data->append(ptr, size * nmemb);
+    return size * nmemb;
+}
 
-        if (!response.error) 
+namespace Http 
+{
+    static std::string Get(const char* url, bool retry = true) 
+    {
+        CURL* curl;
+        CURLcode res;
+        std::string response;
+
+        curl = curl_easy_init();
+        if (curl) 
         {
-            return response.text;
+            curl_easy_setopt(curl, CURLOPT_URL, url);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, fmt::format("Millennium/{}", MILLENNIUM_VERSION).c_str());
+
+            while (true) 
+            {
+                res = curl_easy_perform(curl);
+
+                if (!retry || res == CURLE_OK) 
+                {
+                    break;
+                }
+
+                switch (res) 
+                {
+                    case CURLE_OPERATION_TIMEDOUT: LOG_ERROR("Operation timed out: {}", curl_easy_strerror(res)); break;
+                    case CURLE_COULDNT_CONNECT: LOG_ERROR("Couldn't connect to server: {}", curl_easy_strerror(res)); break;
+                    case CURLE_SSL_CONNECT_ERROR: LOG_ERROR("SSL connection error: {}", curl_easy_strerror(res)); break;
+                    case CURLE_PEER_FAILED_VERIFICATION: LOG_ERROR("Peer failed verification: {}", curl_easy_strerror(res)); break;
+                    case CURLE_GOT_NOTHING: LOG_ERROR("Got nothing: {}", curl_easy_strerror(res)); break;
+                    default: LOG_ERROR("curl_easy_perform() failed: {}", curl_easy_strerror(res)); break;
+                }
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(3));
+            }
+            curl_easy_cleanup(curl);
         }
-        
-        Logger.Log("Couldn't connect to server: [{}]", url);
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        return response;
+    }
+
+    static bool DownloadFile(const char* url, const char* filename) 
+    {
+        CURL* curl;
+        CURLcode res;
+        FILE* file;
+
+        curl = curl_easy_init();
+        if (curl) 
+        {
+            file = fopen(filename, "wb");
+            if (file) 
+            {
+                curl_easy_setopt(curl, CURLOPT_URL, url);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+                curl_easy_setopt(curl, CURLOPT_USERAGENT, fmt::format("Millennium/{}", MILLENNIUM_VERSION).c_str());
+
+                res = curl_easy_perform(curl);
+                fclose(file);
+
+                if (res != CURLE_OK) 
+                {
+                    LOG_ERROR("curl_easy_perform() failed: {}", curl_easy_strerror(res));
+                    return false;
+                }
+            }
+            else 
+            {
+                LOG_ERROR("Failed to open file: {}", filename);
+                return false;
+            }
+            curl_easy_cleanup(curl);
+        }
+        return true;
     }
 }
