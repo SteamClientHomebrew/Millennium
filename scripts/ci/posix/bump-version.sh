246f911331f0388b1298912dd172319204b3d028
shadow <81448108+ShadowMonster99@users.noreply.github.com>
2024-08-02 23:43:30 -0300

    chore(CI): Fix CI issues

--- scripts/ci/posix/bump-version.sh
+++ scripts/ci/posix/bump-version.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-
-# Get the version from semantic-release dry run
 VERSION=$(npx semantic-release --dry-run | grep -oP 'The next release version is \K[0-9.]+')
-
-# Output the version to the console
 echo "Version: $VERSION"
-
-# Set the GitHub Actions output variable
 echo "::set-output name=version::$VERSION"
-
-# Write the version to the file
 echo "# current version of millennium" > ./version
 echo "v$VERSION" >> ./version
