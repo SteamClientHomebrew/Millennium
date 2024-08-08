76b40ffaaec77eccf99ce85a2e30cf9fe05d85b6
shadow <81448108+ShadowMonster99@users.noreply.github.com>
2024-08-08 03:26:13 -0300

    fix: (Linux) Fix python binary paths

--- assets/pipx/config.py
+++ assets/pipx/config.py
@@ -36,7 +36,7 @@ class Config:
         if _platform == "Windows":
             PYTHON_BIN = os.path.join(LOCALS, "cache", "python.exe")  
         elif _platform == "Linux":
-            PYTHON_BIN = os.path.join(os.environ.get("HOME"), ".pyenv", "versions", "3.11.8", "bin", "python")
+            PYTHON_BIN = os.path.join(os.environ.get("HOME"), ".millennium", "ext", "data", "cache", "bin", "python")
 
         PACMAN_LOGS      = os.path.join(LOCALS, "logs", "pacman.log")
         PIP_INSTALL_LOGS = os.path.join(LOCALS, "logs", "pip_boot.log")
--- src/main.cc
+++ src/main.cc
@@ -139,7 +139,7 @@ extern "C"
          * Since this isn't an executable, and is "preloaded", the kernel doesn't implicitly load dependencies, so we need to manually. 
          * libpython3.11.so.1.0 should already be in $PATH, so we can just load it from there.
         */
-        if (!dlopen(fmt::format("{}/.millennium/ext/data/cache/lib/libpython3.11.8.so", homeDirectory).c_str(), RTLD_LAZY | RTLD_GLOBAL)) 
+        if (!dlopen(fmt::format("{}/.millennium/libpython-3.11.8.so", homeDirectory).c_str(), RTLD_LAZY | RTLD_GLOBAL)) 
         {
             LOG_ERROR("Failed to load python libraries: {},\n\nThis is likely because it was not found on disk, try reinstalling Millennium.", dlerror());
         }
