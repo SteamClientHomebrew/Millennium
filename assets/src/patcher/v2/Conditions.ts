0f69dc8bb1b48877cd3f892f1be4809018a04225
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-08-29 19:47:14 +0000

    oops

--- assets/src/patcher/v2/Conditions.ts
+++ assets/src/patcher/v2/Conditions.ts
@@ -1,33 +1,23 @@
-import { pluginSelf } from "millennium-lib";
-import {
-	Conditions,
-	ConditionsStore,
-	ThemeItem,
-	ConditionalControlFlowType as ModuleType,
-} from "../../types";
-import { EvaluatePatch } from "../Dispatch";
+import { pluginSelf } from "millennium-lib"
+import { Conditions, ConditionsStore, ThemeItem, ConditionalControlFlowType as ModuleType } from "../../types"
+import { EvaluatePatch } from "../Dispatch"
 
-export const EvaluateConditions = (
-	theme: ThemeItem,
-	title: string,
-	classes: string[],
-	document: Document,
-): void => {
-	const themeConditions: Conditions = theme.data.Conditions;
-	const savedConditions: ConditionsStore =
-		pluginSelf.conditionals[theme.native];
+export const EvaluateConditions = (theme: ThemeItem, title: string, classes: string[], document: Document): void => {
 
-	for (const condition in themeConditions) {
-		if (!themeConditions.hasOwnProperty(condition)) {
-			return;
-		}
+    const themeConditions: Conditions = theme.data.Conditions
+    const savedConditions: ConditionsStore = pluginSelf.conditionals[theme.native]
 
-		if (condition in savedConditions) {
-			const patch =
-				themeConditions[condition].values[savedConditions[condition]];
+    for (const condition in themeConditions) {
 
-			EvaluatePatch(ModuleType.TargetCss, patch, title, classes, document);
-			EvaluatePatch(ModuleType.TargetJs, patch, title, classes, document);
-		}
-	}
-};
+        if (!themeConditions.hasOwnProperty(condition)) {
+            return 
+        }
+
+        if (condition in savedConditions) {
+            const patch = themeConditions[condition].values[savedConditions[condition]]
+
+            EvaluatePatch(ModuleType.TargetCss, patch, title, classes, document)
+            EvaluatePatch(ModuleType.TargetJs, patch, title, classes, document)
+        }
+    }
+}
