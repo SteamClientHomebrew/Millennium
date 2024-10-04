bb429383d437d02fb71f95efd9d1c4f5cb79db0d
shadow <81448108+shdwmtr@users.noreply.github.com>
2024-10-04 15:18:17 -0300

    chore: Cleanup

--- src/pipes/terminal_pipe.h
+++ src/pipes/terminal_pipe.h
@@ -7,46 +7,48 @@
 #include <sys/log.h>
 #include <fcntl.h>
 
-static const int CreateTerminalPipe()
-{
-    #ifdef _WIN32
+extern "C" {
+    __declspec(dllexport) const int CreateTerminalPipe()
     {
-        HANDLE hPipe = CreateFileA(R"(\\.\pipe\MillenniumStdoutPipe)", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-
-        if (hPipe == INVALID_HANDLE_VALUE)
+        #ifdef _WIN32
         {
-            std::cerr << "Failed to connect to pipe." << std::endl;
-            return 1;
-        }
+            HANDLE hPipe = CreateFileA(R"(\\.\pipe\MillenniumStdoutPipe)", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 
-        int pipeDescriptor = _open_osfhandle((intptr_t)hPipe, _O_WRONLY);
-        if (pipeDescriptor == -1)
-        {
-            std::cerr << "Failed to get pipe file descriptor." << std::endl;
-            CloseHandle(hPipe);
-            return 1;
-        }
+            if (hPipe == INVALID_HANDLE_VALUE)
+            {
+                std::cerr << "Failed to connect to pipe." << std::endl;
+                return 1;
+            }
 
-        FILE* pipeFile = _fdopen(pipeDescriptor, "w");
-        if (!pipeFile)
-        {
-            std::cerr << "Failed to open pipe file descriptor as FILE*." << std::endl;
-            CloseHandle(hPipe);
-            return 1;
-        }
+            int pipeDescriptor = _open_osfhandle((intptr_t)hPipe, _O_WRONLY);
+            if (pipeDescriptor == -1)
+            {
+                std::cerr << "Failed to get pipe file descriptor." << std::endl;
+                CloseHandle(hPipe);
+                return 1;
+            }
 
-        // Redirect stdout to the named pipe
-        if (dup2(_fileno(pipeFile), _fileno(stdout)) == -1)
-        {
-            std::cerr << "Failed to redirect stdout to pipe." << std::endl;
-            CloseHandle(hPipe);
-            fclose(pipeFile); // Close the FILE* on failure
-            return 1;
-        }
-        setvbuf(stdout, NULL, _IONBF, 0);
+            FILE* pipeFile = _fdopen(pipeDescriptor, "w");
+            if (!pipeFile)
+            {
+                std::cerr << "Failed to open pipe file descriptor as FILE*." << std::endl;
+                CloseHandle(hPipe);
+                return 1;
+            }
 
-        std::shared_ptr<FILE*> pipePtr = std::make_shared<FILE*>(pipeFile);
+            // Redirect stdout to the named pipe
+            if (dup2(_fileno(pipeFile), _fileno(stdout)) == -1)
+            {
+                std::cerr << "Failed to redirect stdout to pipe." << std::endl;
+                CloseHandle(hPipe);
+                fclose(pipeFile); // Close the FILE* on failure
+                return 1;
+            }
+            setvbuf(stdout, NULL, _IONBF, 0);
+
+            std::shared_ptr<FILE*> pipePtr = std::make_shared<FILE*>(pipeFile);
+        }
+        #endif
+        return 0;
     }
-    #endif
-    return 0;
 }
