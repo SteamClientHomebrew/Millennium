cfd961c54b59808b625a9dfbe077ae29940998b5
SpaceEnergy <93218131+SpaceEnergy@users.noreply.github.com>
2024-11-24 02:23:35 +0100

    fix: Close installer after successful install
    * change: install.ps1
    
    When the installation is complete, the window is automatically closed.
    
    * change: $message check
    
    * fix: match message
    
    Co-Authored-By: Noxy <107372460+noxygalaxy@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Noxy <107372460+noxygalaxy@users.noreply.github.com>

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -440,6 +440,13 @@ while ($pipeServer.IsConnected) {
     if ($bytesRead -gt 0) {           
         $message = [System.Text.Encoding]::UTF8.GetString($buffer, 0, $bytesRead)
         [Console]::Write($message)
+        
+        # Check if the message contains "SteamUI successfully loaded"
+        if ($message -match "SteamUI successfully loaded!") {
+            Write-Host "`n${BoldGreen}++${ResetColor} Millennium has successfully loaded. Installation complete!"
+            Start-Sleep -Seconds 2
+            exit
+        }
     }
 }
 
