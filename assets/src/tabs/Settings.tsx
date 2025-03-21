7cec364ef4afa0af339b13ade541b3d5ce6d9183
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-03-20 21:22:14 -0300

    chore: Bug fixes

--- assets/src/tabs/Settings.tsx
+++ assets/src/tabs/Settings.tsx
@@ -1,52 +1,23 @@
-import { CSSProperties, useEffect, useState } from 'react'
-import { DialogBodyText, DialogButton, DialogControlsSection, IconsModule, pluginSelf, Toggle, SteamSpinner, Field, callable, ConfirmModal, showModal, } from '@steambrew/client'
+import { useEffect, useState } from 'react'
+import { pluginSelf, Toggle, SteamSpinner, Field, callable } from '@steambrew/client'
 import { locale } from '../locales';
-import { ThemeItem, UpdaterOptionProps } from '../types';
+import { UpdaterOptionProps } from '../types';
 import { Settings } from '../Settings';
 import { ConnectionFailed } from '../custom_components/ConnectionFailed';
 import { SettingsDialogSubHeader } from '../components/ISteamComponents';
 
-const ShowMessageBox = (message: string, title?: string) => {
-    return new Promise((resolve) => {
-        const onOK = () => resolve(true)
-        const onCancel = () => resolve(false)
-
-        showModal(<ConfirmModal
-            // @ts-ignore
-            strTitle={title ?? LocalizationManager.LocalizeString("#InfoSettings_Title")}
-            strDescription={message}
-            onOK={onOK}
-            onCancel={onCancel}
-        />,
-            pluginSelf.windows["Millennium"],
-            {
-                bNeverPopOut: false,
-            },
-        );
-    })
-}
-
-interface FormatString {
-    (template: string, ...args: string[]): string;
-}
-
-const formatString: FormatString = (template, ...args) => {
-    return template.replace(/{(\d+)}/g, (match, index) => {
-        return index < args.length ? args[index] : match;  // Replace {index} with the corresponding argument or leave it unchanged
-    });
-}
-const GetUpdateList = callable<[{ force: boolean }], any>("updater.get_update_list")
-const SetUpdateNotificationStatus = callable<[{ status: boolean }], boolean>("updater.set_update_notifs_status")
-const SetUserWantsUpdates = callable<[{ wantsUpdates: boolean }], void>("MillenniumUpdater.set_user_wants_updates");
-const SetUserWantsNotifications = callable<[{ wantsNotify: boolean }], void>("MillenniumUpdater.set_user_wants_update_notify");
+const GetUpdateList               = callable<[{ force: boolean        }], any>    ("updater.get_update_list")
+const SetUpdateNotificationStatus = callable<[{ status: boolean       }], boolean>("updater.set_update_notifs_status")
+const SetUserWantsUpdates         = callable<[{ wantsUpdates: boolean }], void>   ("MillenniumUpdater.set_user_wants_updates");
+const SetUserWantsNotifications   = callable<[{ wantsNotify: boolean  }], void>   ("MillenniumUpdater.set_user_wants_update_notify");
 
 const SettingsViewModal: React.FC = () => {
 
     const [showUpdateNotifications, setNotifications] = useState<boolean>(undefined)
     const [hasReceivedUpdates, setHasReceivedUpdates] = useState<boolean>(false)
 
-    const [wantsUpdates, setWantsUpdates] = useState(pluginSelf.wantsMillenniumUpdates == UpdaterOptionProps.YES);
-    const [wantsNotify, setWantsNotify] = useState(pluginSelf.wantsMillenniumUpdateNotifications == UpdaterOptionProps.YES);
+    const [wantsUpdates, setWantsUpdates] = useState(pluginSelf.wantsMillenniumUpdates             == UpdaterOptionProps.YES);
+    const [wantsNotify,   setWantsNotify] = useState(pluginSelf.wantsMillenniumUpdateNotifications == UpdaterOptionProps.YES);
 
     const OnUpdateChange = (newValue: boolean) => {
         setWantsUpdates(newValue)
@@ -60,23 +31,19 @@ const SettingsViewModal: React.FC = () => {
         SetUserWantsNotifications({ wantsNotify: newValue });
     }
 
-    const FetchAvailableUpdates = async (): Promise<boolean> => new Promise(async (resolve, reject) => {
+    const FetchSettings = async () => {
         try {
             const updateList = JSON.parse(await GetUpdateList({ force: false }))
             pluginSelf.connectionFailed = false
 
             setNotifications(updateList.notifications ?? false)
             setHasReceivedUpdates(true)
-            resolve(true)
         }
         catch (exception) {
             console.error("Failed to fetch updates")
             pluginSelf.connectionFailed = true
-            reject(false)
         }
-        resolve(true)
-    })
-
+    }
 
     const OnNotificationsChange = async (enabled: boolean) => {
         const result = await SetUpdateNotificationStatus({ status: enabled })
@@ -91,7 +58,7 @@ const SettingsViewModal: React.FC = () => {
         }
     }
 
-    useEffect(() => { FetchAvailableUpdates() }, [])
+    useEffect(() => { FetchSettings() }, [])
 
     /** Check if the connection failed, this usually means the backend crashed or couldn't load */
     if (pluginSelf.connectionFailed) {
@@ -105,10 +72,8 @@ const SettingsViewModal: React.FC = () => {
     return (
         <>
             <SettingsDialogSubHeader>Millennium Updates</SettingsDialogSubHeader>
-
-            <Field label={locale.updatePanelCheckForUpdates}
-                description={locale.toggleWantsMillenniumUpdatesTooltip}
-            >
+            
+            <Field label={locale.updatePanelCheckForUpdates} description={locale.toggleWantsMillenniumUpdatesTooltip}>
                 <Toggle value={wantsUpdates} onChange={OnUpdateChange} />
             </Field>
             <Field
@@ -120,7 +85,6 @@ const SettingsViewModal: React.FC = () => {
             </Field>
 
             <div className="SettingsDialogSubHeader" style={{ marginTop: "20px" }}>Plugin & Theme Updates</div>
-
             <Field
                 label={locale.updatePanelUpdateNotifications}
                 description={locale.updatePanelUpdateNotificationsTooltip}
