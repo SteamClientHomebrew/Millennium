713284e8db33ddef50564fc8c93e23861ec14d1f
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-03-30 00:53:40 -0300

    chore: Update Linux install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 
 # This file is part of the Millennium project.
 # This script is used to install Millennium on a Linux machine.
-# https://github.com/shdwmtr/millennium/blob/main/scripts/install.sh
-# Copyright (c) 2025 Millennium
+# https://github.com/SteamClientHomebrew/Millennium/blob/main/scripts/install.sh
+# Copyright (c) 2024 Millennium
 set -e
 
 BOLD_RED="\033[1;31m"
@@ -16,49 +16,34 @@ BOLD_CYAN="\033[1;36m"
 BOLD_WHITE="\033[1;37m"
 RESET="\033[0m"
 
-for arg in "$@"; do
-    shift
-    case "$arg" in
-        "--root") override_root=1 ;;
-        *)
-        if echo "$arg" | grep -qv "^-"; then
-            tag="$arg"
-        else
-            echo "Invalid option $arg" >&2
-            exit 1
-        fi
-    esac
-done
-
-is_root() {
-    [ "$(id -u)" -ne 0 ]
-}
-
-if ! is_root && [ "${override_root:-0}" -eq 0 ]; then
-    echo "The script was ran under sudo or as root. The script will now exit"
-    echo "If you hadn't intended to do this, please execute the script without root access to avoid problems with spicetify"
-    echo "To override this behavior, pass the '--root' parameter to this script"
-    exit
-fi
-
-
 log() {
     echo -e "$1"
 }
 
+is_root() {
+    [ "$(id -u)" -eq 0 ]
+}
+
+if ! is_root;  then
+    log "${BOLD_RED}!!${RESET} Insufficient permissions. Please run the script as sudo or root"
+    exit 1
+fi
+
 case $(uname -sm) in
     "Linux x86_64") target="linux-x86_64" ;;
-    *) log "Unsupported platform $(uname -sm). x86_64 is the only available platform."; exit ;;
+    *) log "${BOLD_RED}!!${RESET} Unsupported platform $(uname -sm). x86_64 is the only available platform."; exit ;;
 esac
 
+log "resolving dependencies..."
+
 # check for dependencies
-command -v curl >/dev/null || { log "curl isn't installed!" >&2; exit 1; }
-command -v tar >/dev/null || { log "tar isn't installed!" >&2; exit 1; }
-command -v grep >/dev/null || { log "grep isn't installed!" >&2; exit 1; }
-command -v jq >/dev/null || { log "jq isn't installed!" >&2; exit 1; }
+command -v curl >/dev/null || { log "curl isn't installed! Install it from your package manager." >&2; exit 1; }
+command -v tar >/dev/null || { log "tar isn't installed! Install it from your package manager." >&2; exit 1; }
+command -v grep >/dev/null || { log "grep isn't installed! Install it from your package manager." >&2; exit 1; }
+command -v jq >/dev/null || { log "jq isn't installed! Install it from your package manager." >&2; exit 1; }
 
 # download uri
-releases_uri="https://api.github.com/repos/SteamClientHomebrew/Millennium/releases"
+releases_uri="https://api.github.com/repos/shdwmtr/millennium/releases"
 if [ -z "$tag" ]; then
     tag=$(curl -LsH 'Accept: application/vnd.github.v3+json' "$releases_uri" | jq -r '[.[] | select(.prerelease == false)] | .[0].tag_name')
     # get the bytes size of the release assets
@@ -67,101 +52,46 @@ fi
 
 tag=${tag#v}
 size=${size:-0}
+download_uri=https://github.com/shdwmtr/millennium/releases/download/v$tag/millennium-v$tag-$target.tar.gz
 
 # convert bytes to human readable format
 size=$(echo $size | awk '{ split("B KB MB GB TB PB", v); s=1; while ($1 > 1024) { $1 /= 1024; s++ } printf "%.2f %s\n", $1, v[s] }')
 
-log "${BOLD_PINK}++${RESET} Installing Packages (Linux) Millennium@$tag"
-log "\n${BOLD_PINK}::${RESET} Total Download Size: $size"
-log "${BOLD_PINK}::${RESET} Retreiving packages..."
+log "Packages (1) Millennium@$tag-x86_64"
+log "\nTotal Download Size: $size"
+log "Retreiving packages..."
 
-download_uri=https://github.com/SteamClientHomebrew/Millennium/releases/download/v$tag/millennium-v$tag-$target.tar.gz
+# ask to proceed
+echo -e "\n${BOLD_PINK}::${RESET} Installing for \"$SUDO_USER\". Proceed with installation? [Y/n] \c"
+
+read -r proceed
+
+if [ "$proceed" = "n" ]; then
+    exit 1
+fi
 
 # locations
-millennium_install="$HOME/.millennium"
-exe="$millennium_install/ext/bin/millennium"
-bin_dir="$millennium_install/ext/bin"
+millennium_install="/tmp/millennium"
+extract_path="$millennium_install/files"
 tar="$millennium_install/millennium.tar.gz"
 
-# installing
-[ ! -d "$millennium_install" ] && log "${BOLD_PINK}++${RESET} Creating $millennium_install" && mkdir -p "$millennium_install"
+rm -rf "$millennium_install"
+mkdir -p "$millennium_install"
 
-log "\n${BOLD_PINK}(1/2)${RESET} Downloading millennium-v$tag-$target..."
-
-# Download the file with custom progress
+log "receiving packages..."
 curl --fail --location --output "$tar" "$download_uri" --silent
 
-log "${BOLD_PINK}(2/2)${RESET} Unpacking Assets..."
+log "deflating assets..."
+tar xzf "$tar" -C "$extract_path"
 
-tar xzf "$tar" -C "$millennium_install"
+mv "$extract_path/home/user" "$extract_path/home/$SUDO_USER"
+folder_size=$(du -sb "$extract_path" | awk '{print $1}' | numfmt --to=iec-i --suffix=B --padding=7 | sed 's/\([0-9]\)\([A-Za-z]\)/\1 \2/')
+log "\nTotal Install Size: $folder_size"
 
-uncompressed_size=$(echo $(gzip -l "$tar" | awk 'NR==2 {print $2}') \
-    | awk '{ split("B KB MB GB TB PB", v); s=1; while ($1 > 1024) { $1 /= 1024; s++ } printf "%.2f %s\n", $1, v[s] }')
+cp -r "$extract_path"/* / || true
 
+log "cleaning up packages..."
+rm -rf "$millennium_install"
+log "done."
 
-log "\n${BOLD_PINK}::${RESET} Total Installed Size: $uncompressed_size"
-
-log "${BOLD_PINK}::${RESET} Setting up permissions..."
-
-chmod +x "$exe"
-chmod +x "$millennium_install/start.sh"
-chmod +x "$HOME/.millennium/ext/data/cache/bin/python3.11"
-
-log "\n${BOLD_PINK}++${RESET} Cleaning up installer packages..."
-rm "$tar"
-
-notfound() {
-    cat << EOINFO
-Manually add the directory to your \$PATH through your shell profile
-export millennium_install="$millennium_install"
-export PATH="\$PATH:$millennium_install"
-EOINFO
-}
-
-endswith_newline() {
-    [ "$(od -An -c "$1" | tail -1 | grep -o '.$')" = "\n" ]
-}
-
-check() {
-    path="export PATH=\$PATH:$bin_dir"
-    shellrc=$HOME/$1
-
-    if [ "$1" = ".zshrc" ] && [ -n "${ZDOTDIR}" ]; then
-        shellrc=$ZDOTDIR/$1
-    fi
-
-    # Create shellrc if it doesn't exist
-    if ! [ -f "$shellrc" ]; then
-        log "${BOLD_PINK}++${RESET} Creating $shellrc"
-        touch "$shellrc"
-    fi
-
-    # Still checking again, in case touch command failed
-    if [ -f "$shellrc" ]; then
-        if ! grep -q "$bin_dir" "$shellrc"; then
-            log "${BOLD_PINK}++${RESET} Appending $bin_dir to PATH in $shellrc"
-            if ! endswith_newline "$shellrc"; then
-                echo >> "$shellrc"
-            fi
-            echo "${2:-$path}" >> "$shellrc"
-            export PATH="$bin_dir:$PATH"
-        fi
-    else
-        notfound
-    fi
-}
-
-case $SHELL in
-    *zsh) check ".zshrc" ;;
-    *bash)
-        [ -f "$HOME/.bashrc" ] && check ".bashrc"
-        [ -f "$HOME/.bash_profile" ] && check ".bash_profile"
-    ;;
-    *fish) check ".config/fish/config.fish" "fish_add_path $bin_dir" ;;
-    *) notfound ;;
-esac
-
-
-log "${BOLD_PINK}++${RESET} Millennium@v$tag was installed successfully to $millennium_install"
-
-log "\n${BOLD_PINK}::${RESET} To start Millennium along side Steam, run ~/.millennium/start.sh\n   Steam MUST be started from this script to work properly with Millennium.\n   Your base installation of Steam has not been modified, this is simply an extension."
+log "\n${BOLD_PINK}::${RESET} To get started, run \"millennium patch\" to load Millennium along side Steam. Your base installation of Steam has not been modified, this is simply an extension."
