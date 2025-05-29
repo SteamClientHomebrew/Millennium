149aafc62aa4ced4d67422a23e2882c88de3dcce
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-05-29 01:38:51 -0300

    chore: cleanup

--- assets/src/utils/globals.ts
+++ assets/src/utils/globals.ts
@@ -1,6 +1,3 @@
-import { callable } from '@steambrew/client';
-import { PluginComponent } from '../types';
-
 export const API_URL = 'https://steambrew.app';
 export const PLUGINS_URL = [API_URL, 'plugins'].join('/');
 export const THEMES_URL = [API_URL, 'themes'].join('/');
