71b3c10b5f3043df2e2e21d6273af68e21aa869d
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-06-02 03:13:43 -0300

    chore: Fix mk-assets to include other build files

--- scripts/ci/posix/mk-assets.sh
+++ scripts/ci/posix/mk-assets.sh
@@ -4,7 +4,7 @@ cd "/home/runner/work/Millennium/Millennium"
 destinationBase="/home/runner/env/ext/data/assets"
 
 declare -A paths=(
-    ["./assets/.millennium/Dist/index.js"]="$destinationBase/.millennium/Dist/index.js"
+    ["./assets/.millennium/Dist"]="$destinationBase/.millennium/Dist"
     ["./assets/core"]="$destinationBase/core"
     ["./assets/pipx"]="$destinationBase/pipx"
     ["./assets/requirements.txt"]="$destinationBase/requirements.txt"
