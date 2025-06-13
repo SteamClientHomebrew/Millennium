b56db2f87c402b9d3e9960168409dc4f2b15fda4
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-06-12 23:37:16 -0300

    chore(deprecation): Remove deprecation warnings on CI

--- scripts/ci/posix/version.sh
+++ scripts/ci/posix/version.sh
@@ -1,5 +1,5 @@
 VERSION=$(npx semantic-release --dry-run | grep -oP 'The next release version is \K[0-9.]+')
 echo "Version: $VERSION"
-echo "::set-output name=version::$VERSION"
+echo "version=$VERSION" >> "$GITHUB_OUTPUT"
 echo "# current version of millennium" > ./version
 echo "v$VERSION" >> ./version
