906e6a456f951e992a7bfbc2a505c6e84adfaa1f
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-06-21 13:18:57 -0300

    fix: fix Millennium finding incorrect API modules.

--- src/sys/sysfs.cc
+++ src/sys/sysfs.cc
@@ -183,24 +183,21 @@ namespace SystemIO {
 
     MILLENNIUM std::optional<std::string> GetMillenniumPreloadPath()
     {
-        const std::filesystem::path dirPath = std::filesystem::path(GetEnv("MILLENNIUM__SHIMS_PATH"));
-
-        // Regex for files starting with "millennium" and ending with ".js"
-        const std::regex pattern("^millennium.*\\.js$", std::regex_constants::icase);
-
-        for (const auto& entry : std::filesystem::directory_iterator(dirPath))
+        auto dirPath = std::filesystem::path(GetEnv("MILLENNIUM__SHIMS_PATH"));
+        auto preloadPath = dirPath / "millennium.js";
+        
+        if (!std::filesystem::is_directory(dirPath)) 
         {
-            if (entry.is_regular_file())
-            {
-                const std::string filename = entry.path().filename().string();
-                if (std::regex_match(filename, pattern))
-                {
-                    // Ensure forward slashes in the path
-                    return entry.path().generic_string();
-                }
-            }
+            LOG_ERROR("Directory does not exist: {}", dirPath.generic_string());
+            return std::nullopt;
         }
-
-        return std::nullopt;
+        
+        if (!std::filesystem::exists(preloadPath)) 
+        {
+            LOG_ERROR("Preload file does not exist: {}", preloadPath.generic_string());
+            return std::nullopt;
+        }
+        
+        return preloadPath.generic_string();
     }
 }
