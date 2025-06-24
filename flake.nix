0cf03d339893e02e495335ddc3859a746ccef704
Sk7Str1p3 <finenkofedor@gmail.com>
2025-06-24 12:31:18 +0000

    feat: flake init

---
+++ flake.nix
@@ -0,0 +1,34 @@
+# Special thanks to @Sk7Str1p3, @mourogurt, @kaeeraa, @mctrxw for help with this flake and packages
+{
+  description = ''
+    Millennium - an open-source low-code modding framework to create, 
+    manage and use themes/plugins for the desktop Steam Client
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    self.submodules = true; # Requires *Nix* >= 2.27
+  };
+
+  outputs =
+    { nixpkgs, self, ... }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    in
+    {
+      # TODO: move shell to separate file to
+      #       let users without flakes use it too
+      devShells."x86_64-linux".default = pkgs.mkShellNoCC {
+        stdenv = pkgs.stdenv_32bit;
+        name = "Millennium";
+        packages = with pkgs; [
+          nixd
+          nixfmt-rfc-style
+          mypy
+        ];
+      };
+    };
+}
