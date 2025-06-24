1527664d82b47bb193c85b722ac617e27cfce537
Sk7Str1p3 <finenkofedor@gmail.com>
2025-06-24 12:34:43 +0000

    feat: shims nix package

---
+++ nix/shims.nix
@@ -0,0 +1,33 @@
+{
+  stdenv,
+  nodejs,
+  pnpm,
+  faketty,
+}:
+stdenv.mkDerivation rec {
+  pname = "millennium-sdk";
+  version = "git";
+
+  src = ../sdk;
+  pnpmDeps = pnpm.fetchDeps {
+    inherit src version pname;
+    #TODO: automatic hash update
+    hash = "sha256-LofHepVz6CjbAXkUwwNFVzlbmPq+g/gJvkBka9I/gHo=";
+  };
+
+  nativeBuildInputs = [
+    pnpm.configHook
+    nodejs
+    faketty
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    faketty pnpm run build
+  '';
+
+  installPhase = ''
+    mkdir -p $out/share/millennium/shims
+    cp -r typescript-packages/loader/build/* $out/share/millennium/shims
+  '';
+}
