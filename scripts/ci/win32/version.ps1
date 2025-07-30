d580d618954229d61b66fabb390b8f30ec4eaadb
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-07-30 14:33:54 -0300

    feat: Add SignPath test signing

--- scripts/ci/win32/version.ps1
+++ scripts/ci/win32/version.ps1
@@ -1,4 +1,4 @@
 $VERSION = npx semantic-release --dry-run | Select-String -Pattern "The next release version is" | ForEach-Object { $_.ToString() -replace '.*The next release version is ([0-9.]+).*', '$1' }
 Write-Host "Version: $VERSION"
-"version=$version" >> $env:GITHUB_OUTPUT
+"version=$VERSION" >> $env:GITHUB_OUTPUT
 Set-Content -Path ./version -Value "# current version of millennium`nv$VERSION"
