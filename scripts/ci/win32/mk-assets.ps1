6d31cc84ff07a5ed68bfdd82b30bce12cacc4c89
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-09-15 14:24:16 -0300

    chore: update build ci to new backend type

--- scripts/ci/win32/mk-assets.ps1
+++ scripts/ci/win32/mk-assets.ps1
@@ -1,11 +1,7 @@
 $destinationBase = "D:/a/env/ext/data/assets"
 
 $paths = @{
-    "./assets/.millennium/Dist" = "$destinationBase/.millennium/Dist"
-    "./assets/core"             = "$destinationBase/core"
     "./assets/pipx"             = "$destinationBase/pipx"
-    "./assets/requirements.txt" = "$destinationBase/requirements.txt"
-    "./assets/plugin.json"      = "$destinationBase/plugin.json"
 }
 
 foreach ($source in $paths.Keys) {
