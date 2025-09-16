e56ea499db9db9b55c56ba3d8cf404d5d35e5908
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-09-16 02:07:01 -0300

    chore: fix ipc types and unix asset builder

--- scripts/ci/posix/mk-assets.sh
+++ scripts/ci/posix/mk-assets.sh
@@ -3,11 +3,7 @@ destinationBase="${1:-/home/runner/env/ext/data/assets}"
 echo "Copying assets to $destinationBase"
 
 declare -A paths=(
-    ["./assets/.millennium/Dist"]="$destinationBase/.millennium/Dist"
-    ["./assets/core"]="$destinationBase/core"
     ["./assets/pipx"]="$destinationBase/pipx"
-    ["./assets/requirements.txt"]="$destinationBase/requirements.txt"
-    ["./assets/plugin.json"]="$destinationBase/plugin.json"
 )
 
 for source in "${!paths[@]}"; do
