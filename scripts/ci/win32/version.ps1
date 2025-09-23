648e74e51e5b7baf794e366c9e3441eedc3fc136
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-09-23 13:34:09 -0300

    chore: cleanup useless scripts

--- scripts/ci/win32/version.ps1
+++
@@ -1,4 +0,0 @@
-$VERSION = npx semantic-release --no-ci --dry-run | Select-String -Pattern "The next release version is" | ForEach-Object { $_.ToString() -replace '.*The next release version is ([0-9.]+).*', '$1' }
-Write-Host "Version: $VERSION"
-"version=$VERSION" >> $env:GITHUB_OUTPUT
-Set-Content -Path ./version -Value "# current version of millennium`nv$VERSION"
