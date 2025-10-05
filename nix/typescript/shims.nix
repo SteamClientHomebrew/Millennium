7bc588830cae17bb6666ec5715128f90a426ee23
Dan Lock <codenil@proton.me>
2025-10-05 17:25:02 +0100

    Update pnpm hash in shims.nix

--- nix/typescript/shims.nix
+++ nix/typescript/shims.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
   pnpmDeps = pnpm.fetchDeps {
     inherit src version pname;
     #TODO: automatic hash update
-    hash = "sha256-LofHepVz6CjbAXkUwwNFVzlbmPq+g/gJvkBka9I/gHo=";
+    hash = "sha256-ayWGpybXux0BY3CojkPK3fCGUq9mmawoKuddN2Fb5OA=";
     fetcherVersion = 2;
   };
 
