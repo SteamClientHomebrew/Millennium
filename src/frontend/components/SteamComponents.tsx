30af27d86b96b36891d501b365aa329d75d4c76c
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-11-18 18:47:16 -0400

    fix: beta fixes

--- src/frontend/components/SteamComponents.tsx
+++ src/frontend/components/SteamComponents.tsx
@@ -42,7 +42,7 @@ interface BBCodeParserProps {
 }
 
 export const BBCodeParser: FC<BBCodeParserProps> = findModuleExport(
-	(m) => typeof m === 'function' && m.toString().includes('ElementAccumulator') && m.toString().includes('parser.ParseBBCode'),
+	(m) => typeof m === 'function' && m?.toString?.().includes('ElementAccumulator') && m?.toString?.().includes('parser.ParseBBCode'),
 ) as FC<BBCodeParserProps>;
 
 interface SettingsDialogSubHeaderProps extends React.HTMLProps<HTMLDivElement> {
@@ -55,40 +55,41 @@ export const Separator: React.FC = () => <div className={fieldClasses.Standalone
 
 export const DesktopTooltip = findModuleDetailsByExport(
 	(m) =>
-		m?.toString()?.includes(`divProps`) &&
-		m?.toString()?.includes(`tooltipProps`) &&
-		m?.toString()?.includes(`toolTipContent`) &&
-		m?.toString()?.includes(`tool-tip-source`),
+		m?.toString?.()?.includes(`divProps`) &&
+		m?.toString?.()?.includes(`tooltipProps`) &&
+		m?.toString?.()?.includes(`toolTipContent`) &&
+		m?.toString?.()?.includes(`tool-tip-source`),
 )?.[1];
 
 export const createWindowContext = findModuleDetailsByExport(
 	(m) =>
-		m?.toString()?.includes('current?.params.bNoInitialShow') &&
-		m?.toString()?.includes('current?.params.bNoFocusOnShow') &&
-		m?.toString()?.includes('current.m_callbacks'),
+		m?.toString?.()?.includes('current?.params.bNoInitialShow') &&
+		m?.toString?.()?.includes('current?.params.bNoFocusOnShow') &&
+		m?.toString?.()?.includes('current.m_callbacks'),
 )?.[1];
 
 export const ModalManagerInstance = findModuleDetailsByExport((m) => {
-	return m?.hasOwnProperty('m_mapModalManager');
+	return m?.hasOwnProperty?.('m_mapModalManager');
 })?.[1];
 
 export const windowHandler = findModuleDetailsByExport(
-	(m) => m?.toString()?.includes('SetCurrentLoggedInAccountID') && m?.toString()?.includes('bIgnoreSavedDimensions') && m?.toString()?.includes('updateParamsBeforeShow'),
+	(m) =>
+		m?.toString?.()?.includes('SetCurrentLoggedInAccountID') && m?.toString?.()?.includes('bIgnoreSavedDimensions') && m?.toString?.()?.includes('updateParamsBeforeShow'),
 )?.[1];
 
-export const RenderWindowTitle = findModuleDetailsByExport((m) => m?.toString()?.includes('title-bar-actions window-controls'))?.[1];
+export const RenderWindowTitle = findModuleDetailsByExport((m) => m?.toString?.()?.includes('title-bar-actions window-controls'))?.[1];
 
 export const g_ModalManager = findModuleDetailsByExport((m) => {
-	return m?.toString()?.includes('useContext') && m?.toString()?.includes('ModalManager') && m?.length === 0;
+	return m?.toString?.()?.includes('useContext') && m?.toString?.()?.includes('ModalManager') && m?.length === 0;
 })?.[1];
 
 export const OwnerWindowRef = findModuleDetailsByExport(
 	(m) =>
-		m?.toString()?.includes('ownerWindow') &&
-		m?.toString()?.includes('children') &&
-		m?.toString()?.includes('useMemo') &&
-		m?.toString()?.includes('Provider') &&
-		!m?.toString()?.includes('refFocusNavContext'),
+		m?.toString?.()?.includes('ownerWindow') &&
+		m?.toString?.()?.includes('children') &&
+		m?.toString?.()?.includes('useMemo') &&
+		m?.toString?.()?.includes('Provider') &&
+		!m?.toString?.()?.includes('refFocusNavContext'),
 )?.[1];
 
 export const popupModalManager: any = Object.values(findModuleDetailsByExport((m) => m?.toString()?.includes('GetContextMenuManagerFromWindow'))?.[0] || {})?.find(
@@ -97,15 +98,15 @@ export const popupModalManager: any = Object.values(findModuleDetailsByExport((m
 
 export const contextMenuManager = findModuleDetailsByExport(
 	(m) =>
-		m?.toString()?.includes('CreateContextMenuInstance') &&
+		m?.toString?.()?.includes('CreateContextMenuInstance') &&
 		m?.prototype?.hasOwnProperty('GetVisibleMenus') &&
 		m?.prototype?.hasOwnProperty('GetAllMenus') &&
 		m?.prototype?.hasOwnProperty('ShowMenu'),
 )?.[1];
 
 export const PopupModalHandler: any = Object.values(
-	findModuleDetailsByExport((m) => m?.toString()?.includes('this.context.callbacks?.onDisabledItemSelected'))?.[0] || {},
-)?.find((m) => m?.toString()?.includes('useId'));
+	findModuleDetailsByExport((m) => m?.toString?.()?.includes('this.context.callbacks?.onDisabledItemSelected'))?.[0] || {},
+)?.find((m) => m?.toString?.()?.includes('useId'));
 
 interface GenericConfirmDialogProps {
 	children: ReactNode;
