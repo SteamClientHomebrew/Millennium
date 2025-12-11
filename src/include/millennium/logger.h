9f834925417cc1f0d642fa118f9658db97f44846
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-12-11 11:36:02 -0400

    chore: fix gcc LOG_ERROR

--- src/include/millennium/logger.h
+++ src/include/millennium/logger.h
@@ -101,6 +101,7 @@ extern OutputLogger Logger;
 #define PRETTY_FUNCTION __PRETTY_FUNCTION__
 #endif
 
+#if defined(__clang__)
 #ifndef LOG_ERROR
 #define LOG_ERROR(...) Logger.ErrorTrace(FIRST(__VA_ARGS__), __FILE__, __LINE__, PRETTY_FUNCTION REST(__VA_ARGS__))
 #define FIRST(...) FIRST_HELPER(__VA_ARGS__, throwaway)
@@ -110,8 +111,9 @@ extern OutputLogger Logger;
 #define REST_HELPER2(qty, ...) REST_HELPER_##qty(__VA_ARGS__)
 #define REST_HELPER_ONE(first)
 #define REST_HELPER_TWOORMORE(first, ...) , __VA_ARGS__
-#define NUM(...) \
-    SELECT_10TH(__VA_ARGS__, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE,\
-        TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, ONE, throwaway)
+#define NUM(...) SELECT_10TH(__VA_ARGS__, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, ONE, throwaway)
 #define SELECT_10TH(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, ...) a10
+#endif
+#else
+#define LOG_ERROR(fmt, ...) Logger.ErrorTrace(fmt, __FILE__, __LINE__, PRETTY_FUNCTION, ##__VA_ARGS__)
 #endif
