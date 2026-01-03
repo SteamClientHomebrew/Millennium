d910d86d784b5575d283f295e427a2c7089c7ad0
trivaris <git@trivaris.org>
2026-01-03 19:59:19 +0100

    feat: Flake Init

---
+++ packages/nix/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Nix Build for Millennium";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    millennium-src.url = "github:SteamClientHomebrew/millennium/91bd5b530859db93857c3b0908e81809b5f3907e";
+    millennium-src.flake = false;
+  };
+
+  outputs = { self, nixpkgs, millennium-src }: {
+    packages.x86_64-linux = {
+
+    };
+  };
+}
