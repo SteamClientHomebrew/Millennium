ed7d8aa0a8d573a0d23cf2d904b7037afc9706d8
Ethan Alexander <81448108+shdwmtr@users.noreply.github.com>
2026-05-22 21:08:40 -0300

    fix: Fix Field on latest Steam client breaking Millennium settings page

--- src/sdk/packages/client/src/components/Item.ts
+++ src/sdk/packages/client/src/components/Item.ts
@@ -1,13 +1,14 @@
 import { ReactNode } from 'react';
 
 export interface ItemProps {
-  label?: ReactNode;
-  description?: ReactNode;
-  children?: ReactNode;
-  layout?: 'below' | 'inline';
-  icon?: ReactNode;
-  bottomSeparator?: 'standard' | 'thick' | 'none';
-  indentLevel?: number;
-  tooltip?: string;
-  highlightOnFocus?: boolean;
+    label?: ReactNode;
+    description?: ReactNode;
+    children?: ReactNode;
+    layout?: 'below' | 'inline';
+    icon?: ReactNode;
+    bottomSeparator?: 'standard' | 'thick' | 'none';
+    childrenContainerWidth?: 'min' | 'max' | 'fixed'; // Does not work with layout==='below'
+    indentLevel?: number;
+    tooltip?: string;
+    highlightOnFocus?: boolean;
 }
